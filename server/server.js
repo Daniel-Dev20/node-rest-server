b9473392a43459735886137eab3fee526fe10cd0
Daniel <arevalodev20@gmail.com>
2021-01-25 22:36:52 -0600

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,31 @@
+
+require('./config/config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 4040;
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    res.json('Hola Mundo');
+});
+
+app.put('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+
+    res.json({
+        id
+    })
+});
+
+app.post('/usuario', (req, res) => {
+    let body = req.body;
+    res.json({
+        Usuario: body
+
+    })
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando peticiones en el puerto',process.env.PORT);
+})
