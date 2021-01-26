4d445254778e8c383182cfb6033c69eccf25edde
Daniel <arevalodev20@gmail.com>
2021-01-25 23:13:34 -0600

    modificaciones en archivo server

--- server/server.js
+++ server/server.js
@@ -20,10 +20,7 @@ app.put('/usuario/:id', (req, res) => {
 
 app.post('/usuario', (req, res) => {
     let body = req.body;
-    res.json({
-        Usuario: body
-
-    })
+    res.send('get usuario')
 });
 
 app.listen(process.env.PORT, () => {
